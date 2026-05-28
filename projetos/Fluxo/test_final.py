e1a96d14c55181de374f35622f7b94af938817a7
Josalison <josalison.vit@gmail.com>
2026-05-28 17:10:34 -0300

    fix: ajusta escopo do scenarios para o github actions

--- projetos/Fluxo/test_final.py
+++ projetos/Fluxo/test_final.py
@@ -1,8 +1,7 @@
 from pytest_bdd import scenarios
 import projetos.Fluxo.steps.steps
 
-# Puxa os cenários do BDD
-scenarios('./projetos/Fluxo/features')
-# O gancho do teste fica 100% vazio, sem receber (page) e sem comandos dentro!
+
+scenarios('features')
 def test_fluxo():
     pass
