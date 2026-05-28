e1c286bd6217ab1dd18e7e68c0642a859f3f1781
Josalison <josalison.vit@gmail.com>
2026-05-28 17:05:02 -0300

    fix: altera caminho do scenarios para relativo universal

--- projetos/Fluxo/test_final.py
+++ projetos/Fluxo/test_final.py
@@ -2,8 +2,7 @@ from pytest_bdd import scenarios
 import projetos.Fluxo.steps.steps
 
 # Puxa os cenários do BDD
-scenarios('C:/Users/josam/OneDrive/Área de Trabalho/testes/projetos/Fluxo/features')
-
+scenarios('./projetos/Fluxo/features')
 # O gancho do teste fica 100% vazio, sem receber (page) e sem comandos dentro!
 def test_fluxo():
     pass
